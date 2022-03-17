096eb954f47229e1b83ff05079713c8877625310
Vikram1997-hue <vikramaditya.2207@gmail.com>
2022-03-17 21:40:14 +0530

    input of target folder path

---
+++ fileOrganizer.js
@@ -0,0 +1,61 @@
+/*For people who hate the tiresome job of maintaining their files and folders in an organized fashion, I present a FILE ORGANIZER
+MADE IN NODE JS. (plus shit from the LPU kid's post)
+The rules are simple: copy the file "fileOrganiser.js" (link: <GitHub link>) to whichever folder you wanna organize, and then 
+run fileOrganizer.js. That's it.
+Shoutout to Mohd Aman sir
+*/
+
+let fs = require('fs');
+let path = require('path');
+
+let input = process.argv[2];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let audio = path.join(mydir,"Audio");
+// let video = path.join(mydir,"Videos");
+// let text = path.join(mydir,"Text Files");
+// let image = path.join(mydir,"Images");
+// let docs = path.join(mydir,"Documents");
+// let sw = path.join(mydir,"Applications");
+// let others = path.join(__dirname, "Others");
+
+// let arr = [audio, video, text, image, docs, sw, others];
+
+// let arrAudio = [".mp3", ".aac", ".wav", ".flac", ".ogg"];
+// let arrVideo = [".mp4", ".mov", ".avi", ".wmv", ".mkv"];
+// let arrText = [".txt"];
+// let arrImage = [".jpeg", ".jpg", ".png", ".heic", ".heif", ".img"];
+// let arrDocs = [".doc", ".pdf", ".xlsx", ".xls", ".docx"];
+// let arrSw = [".apk", ".exe", ""];
+// let arrOthers = [];
+
