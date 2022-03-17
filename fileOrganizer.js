eede27d62743712505944d44ae901f3a3af60ad6
Vikram1997-hue <ranterdudewtd@gmail.com>
2022-03-17 23:16:34 +0530

    final

--- fileOrganizer.js
+++ fileOrganizer.js
@@ -13,107 +13,62 @@ let folderPath = process.argv[2];
 //error handling - check if provided path is valid
 if(!fs.existsSync(folderPath))
     console.log("Please enter a valid path!");
-
-let extnsList = {
-    Audio: [".mp3", ".aac", ".wav", ".flac", ".ogg"],
-    Videos: [".mp4", ".mov", ".avi", ".wmv", ".mkv"],
-    Text: [".txt"],
-    Images: [".jpeg", ".jpg", ".png", ".heic", ".heif", ".img", ".webp", ".gif"],
-    Documents: [".doc", ".pdf", ".xlsx", ".xls", ".docx"],
-    Software: [".apk", ".exe"]
-};
+else {
 
 
+    let extnsList = {
+        Audio: [".mp3", ".aac", ".wav", ".flac", ".ogg"],
+        Videos: [".mp4", ".mov", ".avi", ".wmv", ".mkv"],
+        Text: [".txt"],
+        Images: [".jpeg", ".jpg", ".png", ".heic", ".heif", ".img", ".webp", ".gif"],
+        Documents: [".doc", ".pdf", ".xlsx", ".xls", ".docx"],
+        Software: [".apk", ".exe"]
+    };
 
 
-let files = fs.readdirSync(folderPath);
 
-for(let i=0 ; i<files.length ; i++) {
 
-    let ext = path.extname(files[i]);
-    let nameOfFolder = giveFolderName(ext);
-    console.log(files[i],"--->",nameOfFolder);
+    let files = fs.readdirSync(folderPath);
 
-    let myFolder = path.join(folderPath, nameOfFolder);
-    if(!fs.existsSync(myFolder))
-        fs.mkdirSync(myFolder);
+    for(let i=0 ; i<files.length ; i++) {
 
-    let filePath = path.join(folderPath, files[i]);
+        let ext = path.extname(files[i]);
+        let nameOfFolder = giveFolderName(ext);
+        console.log(files[i],"--->",nameOfFolder);
+
+        let myFolder = path.join(folderPath, nameOfFolder);
+        if(!fs.existsSync(myFolder))
+            fs.mkdirSync(myFolder);
+
+        let filePath = path.join(folderPath, files[i]);
+        
+
+
+        //moving part begins
+        let src = filePath;
+        let dest = path.join(myFolder,files[i]);
+        console.log("Src:",src);
+        console.log("Dest:",dest,"\n");
+        fs.copyFileSync(src, dest);
+        fs.unlinkSync(src);
+
+    }
+
+
+    function giveFolderName(ext) { 
+
+        for(let key in extnsList) { //key becomes "Audio", then "Video", then "Text",... "Software"
     
-
-
-    //moving part begins
-    let src = filePath;
-    let dest = path.join(myFolder,files[i]);
-    console.log("Src:",src);
-    console.log("Dest:",dest,"\n");
-    fs.copyFileSync(src, dest);
-    fs.unlinkSync(src);
-
+            let tempArr = extnsList[key];
+            for(let i=0 ; i<tempArr.length ; i++) {
+                
+                if(ext == tempArr[i])
+                    return key;
+            }
+        }
+        return "Others";
+    };
+    
 }
 
-
-
-function giveFolderName(ext) { 
-
-    for(let key in extnsList) { //key becomes "Audio", then "Video", then "Text",... "Software"
-
-        let tempArr = extnsList[key];
-        for(let i=0 ; i<tempArr.length ; i++) {
-            
-            if(ext == tempArr[i])
-                return key;
-        }
-    }
-    return "Others";
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let audio = path.join(mydir,"Audio");
-// let video = path.join(mydir,"Videos");
-// let text = path.join(mydir,"Text Files");
-// let image = path.join(mydir,"Images");
-// let docs = path.join(mydir,"Documents");
-// let sw = path.join(mydir,"Applications");
-// let others = path.join(__dirname, "Others");
-
-// let arr = [audio, video, text, image, docs, sw, others];
-
-// let arrAudio = [".mp3", ".aac", ".wav", ".flac", ".ogg"];
-// let arrVideo = [".mp4", ".mov", ".avi", ".wmv", ".mkv"];
-// let arrText = [".txt"];
-// let arrImage = [".jpeg", ".jpg", ".png", ".heic", ".heif", ".img"];
-// let arrDocs = [".doc", ".pdf", ".xlsx", ".xls", ".docx"];
-// let arrSw = [".apk", ".exe", ""];
-// let arrOthers = [];
 
